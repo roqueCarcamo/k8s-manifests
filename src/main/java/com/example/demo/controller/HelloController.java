346f1ba0ae8049ed91000f37dcca2701030b5030
Rodolfo Roque Cárcamo Mesa <carcamomesa@gmail.com>
2024-10-15 03:52:49 +0000

    Prueba logs

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,11 +2,18 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 class HelloController {
+
+    @PostConstruct
+    public void init(){
+        log.info("RAMA MAIN");
+    } 
 
     @GetMapping("/")
     public String hello() {
